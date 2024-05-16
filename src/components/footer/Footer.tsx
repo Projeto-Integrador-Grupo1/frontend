4a4f9d64ab6ffd2833d961915cd374b1058667c2
VncioFranco <151756433+VncioFranco@users.noreply.github.com>
2024-05-16 18:46:08 -0300

    correção footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,20 +1,20 @@
 import {
-  FacebookLogo,
-  InstagramLogo,
-  LinkedinLogo,
+  GithubLogo
+ 
 } from "@phosphor-icons/react"
 
 function Footer() {
   return (
     <>
-      <div className="flex justify-center bg-indigo-900 text-white">
-        <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">Zerone</p>
-          <p className="text-lg">Acesse nossas redes sociais</p>
-          <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <InstagramLogo size={48} weight="bold" />
-            <FacebookLogo size={48} weight="bold" />
+      <div className="flex justify-center bg-blue-500 text-white">
+        <div className="container flex justify-around  items-center py-4">
+          <p className="text-xl font-bold">© 2024 Zerone | Todos os direitos reservados.</p>
+          <div className="flex items-center space-x-4">
+            <p className="text-lg">Veja nosso projeto no Github:
+            </p>
+            <a href="https://github.com/Projeto-Integrador-Grupo1/frontend" target="_blank" rel="">
+              <GithubLogo size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
