7ad52b1b8612b0f4bfb133fd0e06d9f8622a9457
Alan Alves <alan.alvess@outlook.com>
2024-06-20 22:06:55 -0300

    Adicionando responsividade ao navbar

---
+++ src/components/navbarComponent/NavbarComponent.tsx
@@ -0,0 +1,103 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Button, DarkThemeToggle, Flowbite, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { SignIn } from "@phosphor-icons/react";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
+import DropdownPerfil from "./dropdownPerfil/DropdownPerfil";
+import imgLogo from "../../assets/img/zerone-logo.png";
+
+function NavbarComponent() {
+
+  const { usuario } = useContext(AuthContext);
+
+  let navbarComponent;
+
+  if (usuario.token !== "") {
+    navbarComponent = (
+      <div className="py-10">
+        <Navbar className="fixed w-full z-50 top-0 start-0 bg-azul-200" fluid >
+          <NavbarBrand >
+            <Link to="/home">
+              <img src={imgLogo} className="mr-4 rounded-xl bg-cinza-100 w-32 lg:w-44 " alt="Logo Zerone" />
+            </Link>
+          </NavbarBrand>
+
+          <div className="flex md:order-2">
+            <NavbarToggle />
+
+            <DropdownPerfil />
+
+            <Flowbite >
+              <DarkThemeToggle className="bg-azul-100 mx-4 dark:bg-preto-400" />
+            </Flowbite>
+          </div>
+
+          <NavbarCollapse className=" text-2xl">
+            <NavbarLink>
+              <Link to="/projetos" className="text-xl hover:underline focus:text-white" >
+                Projetos
+              </Link>
+            </NavbarLink>
+
+            <NavbarLink >
+              <Link to="/sobre" className="text-xl hover:underline focus:text-white" >
+                Quem somos
+              </Link>
+            </NavbarLink>
+
+          </NavbarCollapse>
+        </Navbar>
+
+      </div>
+    )
+  } else {
+    navbarComponent = (
+
+      <Navbar className="fixed w-full z-50 top-0 start-0 bg-azul-200" fluid >
+        <NavbarBrand >
+          <Link to="/home">
+            <img src={imgLogo} className="mr-4 rounded-xl bg-cinza-100 w-32 lg:w-44 " alt="Logo Zerone" />
+          </Link>
+        </NavbarBrand>
+
+        <div className="flex md:order-2">
+          <NavbarToggle />
+
+          <Button size="xs" >
+            <Link to="/login" className="flex items-center text-xl bg-rosa-200  p-2 rounded-lg hover:bg-azul-100 hover:text-rosa-200">
+              <SignIn size={26} /> Login
+            </Link>
+          </Button>
+
+          <Flowbite >
+            <DarkThemeToggle className="bg-azul-100 mx-4 dark:bg-preto-400" />
+          </Flowbite>
+        </div>
+
+        <NavbarCollapse className=" text-2xl">
+          <NavbarLink>
+            <Link to="/projetos" className="text-xl hover:underline focus:text-white" >
+              Projetos
+            </Link>
+          </NavbarLink>
+
+          <NavbarLink >
+            <Link to="/sobre" className="text-xl hover:underline focus:text-white" >
+              Quem somos
+            </Link>
+          </NavbarLink>
+        </NavbarCollapse>
+
+      </Navbar>
+    )
+  }
+  return (
+    <>
+      {navbarComponent}
+    </>
+  )
+}
+
+export default NavbarComponent;
---
+++ src/components/navbarComponent/dropdownPerfil/DropdownPerfil.tsx
@@ -0,0 +1,73 @@
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Avatar, Dropdown } from "flowbite-react";
+
+import { AuthContext } from '../../../contexts/AuthContext';
+import { ToastAlert, Toast } from '../../../utils/ToastAlert';
+
+function DropdownPerfil() {
+  const navigate = useNavigate();
+  const { usuario, handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    ToastAlert('Usuário deslogado com sucesso', Toast.Sucess);
+    navigate('/login');
+  }
+
+  function iniciaisNome(): JSX.Element {
+    const nomes = usuario.nome.split(' ');
+    const inicialPrimeiroNome = nomes[0][0];
+    const inicialUltimoNome = nomes.length > 1 ? nomes[nomes.length - 1][0] : '';
+    const iniciais = `${inicialPrimeiroNome}${inicialUltimoNome}`.toUpperCase();
+
+    if (usuario.foto && usuario.foto.trim() !== '') {
+      return <Avatar img={usuario.foto} rounded />
+    }
+
+    return <Avatar placeholderInitials={iniciais} rounded />
+  }
+
+  return (
+    <>
+      <Dropdown
+        arrowIcon={false}
+        inline
+        label={iniciaisNome()}
+      >
+        <Dropdown.Header>
+          <span className="block text-sm">{usuario.nome}</span>
+          <span className="block truncate text-sm font-medium">{usuario.email}</span>
+        </Dropdown.Header>
+
+        <Link to={`/perfil`} className="hover:underline">
+          <Dropdown.Item>
+            Meu Perfil
+          </Dropdown.Item>
+        </Link>
+
+        <Link to="/meusProjetos" className="hover:underline">
+          <Dropdown.Item>
+            Meus projetos
+          </Dropdown.Item>
+        </Link>
+
+        <Link to="/categorias" className="hover:underline">
+          <Dropdown.Item>
+            Categorias
+          </Dropdown.Item>
+        </Link>
+
+        <Dropdown.Divider />
+
+        <Link to="/home" onClick={logout} className="hover:underline">
+          <Dropdown.Item>
+            Sair
+          </Dropdown.Item>
+        </Link>
+      </Dropdown>
+    </>
+  )
+}
+
+export default DropdownPerfil;
