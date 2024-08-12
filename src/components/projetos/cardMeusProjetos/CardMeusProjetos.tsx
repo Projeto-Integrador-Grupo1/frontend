e9a7dfaa34a6b68af68f61b8b4900900f7ace2b3
Alan Alves <alan.alvess@outlook.com>
2024-08-12 19:25:17 -0300

    Implementando verificação de valores deletados antes de excluir um projeto.

--- src/components/projetos/cardMeusProjetos/CardMeusProjetos.tsx
+++ src/components/projetos/cardMeusProjetos/CardMeusProjetos.tsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { Avatar, Button, Card } from "flowbite-react";
+import { Avatar, Button, Card, Modal } from "flowbite-react";
 import { ArrowRight, Trash } from "@phosphor-icons/react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 import { AuthContext } from "../../../contexts/AuthContext";
 
@@ -15,6 +16,8 @@ function CardMeusProjetos({ projeto }: CardMeusProjetosProps) {
 
   const { usuario } = useContext(AuthContext);
 
+  const [openModal, setOpenModal] = useState(false);
+
   const nomes = projeto.usuario.nome.split(' ');
   const inicialPrimeiroNome = nomes[0][0];
   const inicialUltimoNome = nomes.length > 1 ? nomes[nomes.length - 1][0] : '';
@@ -26,7 +29,97 @@ function CardMeusProjetos({ projeto }: CardMeusProjetosProps) {
     dateStyle: "medium",
   }).format(dataDoBanco);
 
-  let cardComponent;
+  let cardComponent: any;
+
+  let buttonDeleteComponent: any;
+
+  if (projeto.valorAtual === 0) {
+    buttonDeleteComponent = (
+      <Link to={`/deletarProjeto/${projeto.id}`}>
+        <Button className=" px-2 bg-red-500">
+          <Trash size={20} />
+        </Button>
+      </Link>
+    )
+
+  } else {
+
+    if (projeto.valorAtual === projeto.valorMeta) {
+
+      buttonDeleteComponent = (
+        <>
+          <Button className=" px-2 bg-red-500" onClick={() => setOpenModal(true)}>
+            <Trash size={20} />
+          </Button>
+
+          <Modal show={openModal} size="md" onClose={() => setOpenModal(false)} popup dismissible>
+            <Modal.Header />
+            <Modal.Body>
+              <div className="text-center">
+                <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-red-600 " />
+                <h3 className="mb-5 text-lg font-normal text-gray-900 dark:text-cinza-100">
+                  Este projeto possui valores a receber.
+                </h3>
+                <p className="mb-5 text-gray-500 dark:text-gray-400">
+                  Para concluir a exclusão, realize o saque dos valores recebidos.
+                </p>
+                <div className="flex justify-center gap-4">
+                  <Button color="gray" onClick={() => setOpenModal(false)}>
+                    <Link className="flex justify-center items-center  font-semibold dark:text-cinza-100" to={`/projeto/${projeto.id}`}>
+                      Ver meu projeto
+                      <ArrowRight className="-mr-1 mt-1 ml-2 h-4 w-4 " size={20} />
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </>
+      )
+    } else {
+      buttonDeleteComponent = (
+        <>
+          <Button className=" px-2 bg-red-500" onClick={() => setOpenModal(true)}>
+            <Trash size={20} />
+          </Button>
+
+          <Modal show={openModal} size="xl" onClose={() => setOpenModal(false)} popup dismissible >
+            <Modal.Header />
+            <Modal.Body>
+              <div className="text-center">
+                <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-red-600 " />
+                <h3 className="mb-5 text-lg font-normal text-gray-900 dark:text-cinza-100">
+                  Este projeto possui valores a receber, porém ainda não atingiu a meta de arrecadação.
+                </h3>
+
+                <ul className=" list-disc p-5 text-left dark:text-cinza-100">
+                  <li> Não será possível receber o valor arrecadado até o momento;</li>
+                  <li> Não será possível receber outras doações para este projeto;</li>
+                  <li> Este projeto será deletado da base de dados.</li>
+                </ul>
+
+                <p className="text-rosa-200 dark:text-azul-200 text-xs mb-5">
+                  Obs.: Os valores arrecadaddos até o momento, será inteiramente devolvido aos doadores.
+                </p>
+
+                <div className="flex justify-center gap-4">
+                  <Link to={`/deletarProjeto/${projeto.id}`}>
+                    <Button className=" px-2 bg-red-500">
+                      Excluir
+                    </Button>
+                  </Link>
+                  <Button color="gray" onClick={() => setOpenModal(false)}>
+                    <span>Cancelar</span>
+                  </Button>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
+        </>
+      )
+    }
+
+  }
 
   if (usuario.token !== "" && usuario.email === projeto.usuario?.email) {
     cardComponent = (
@@ -58,7 +151,7 @@ function CardMeusProjetos({ projeto }: CardMeusProjetosProps) {
               Apoiadores: {projeto.qtdDoacoes}
             </p>
             <p className="font-semibold text-sm text-gray-600 dark:text-gray-400 my-0">
-            Última Atualização: {dataLocal}
+              Última Atualização: {dataLocal}
             </p>
           </div>
 
@@ -73,11 +166,8 @@ function CardMeusProjetos({ projeto }: CardMeusProjetosProps) {
 
             </Link>
 
-            <Link to={`/deletarProjeto/${projeto.id}`}>
-              <Button className=" px-2 bg-red-500">
-                <Trash size={20} />
-              </Button>
-            </Link>
+            {buttonDeleteComponent}
+
 
           </div>
           <Link className="flex justify-center items-center pt-4 font-semibold dark:text-cinza-100" to={`/projeto/${projeto.id}`}>
